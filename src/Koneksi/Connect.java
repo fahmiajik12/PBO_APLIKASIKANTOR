3fb3a1e7237c77959ee2c009bb7ee7944c960bd8
fahmiajik12 <fahmifak@gmail.com>
2022-07-06 17:21:09 +0700

    APLIKASI KANTOR PBO

---
+++ src/Koneksi/Connect.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Koneksi;
+import com.mysql.jdbc.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author user
+ */
+public class Connect {
+    private static Connection koneksi;
+
+ public static Connection GetConnection() throws SQLException{
+ if(koneksi==null){
+ new Driver();
+
+koneksi=DriverManager.getConnection("jdbc:mysql://localhost/kantor","root","");
+ }
+ return koneksi;
+}
+}
